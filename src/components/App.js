274a7d9910581a94b1797363a8c0bfdbe58829e7
Wasanta <kwan_u_day@hotmail.com>
2021-12-02 14:21:00 -0500

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
+const containerSize = 4
+const startBudget = 100
 
 function App() {
+
+  const [allSushi, setSushi] = useState([])
+  const [sushiStart, setSushiStart] = useState(0)
+  const [budget, setBudget] = useState(startBudget)
+
+  useEffect(() => {
+    fetch(API)
+    .then(res => res.json())
+    .then(data => setSushi(data))
+  },[])
+
+  function showMore(){
+    setSushiStart(sushiStart+containerSize)
+  }
+
+  function eatSushi(piece){
+
+    if(piece.eaten || budget - piece.price < 0){
+      return;
+    }
+
+    setBudget(budget - piece.price)
+
+    setSushi(
+      allSushi.map(sushi => sushi.id === piece.id? {...piece, eaten: true} : sushi)
+    )
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer eatSushi={eatSushi} showMore={showMore} sushis={allSushi.slice(sushiStart,sushiStart+containerSize)}/>
+      <Table budget={budget} plates = {allSushi.filter(piece => piece.eaten)}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,18 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({sushis, showMore, eatSushi}) {
   return (
     <div className="belt">
       {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushis.map(sushi => (   
+      <Sushi 
+        key={sushi.id} 
+        sushi={sushi}
+        eatSushi ={eatSushi}
+      />))} 
+      <MoreButton handleClick = {showMore}/>
     </div>
   );
 }
